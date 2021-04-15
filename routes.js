015b1d172db3cc99046a1babce7b92b0af741f0b
Michael Carr <xyeres@me.com>
2021-04-15 14:43:57 -0700

    add get /users

--- routes.js
+++ routes.js
@@ -3,12 +3,20 @@
 const express = require('express');
 const { asyncHandler } = require('./middleware/async-handler');
 const { User } = require('./models');
+const { authenticateUser } = require('./middleware/auth-user');
 
 // Construct a router instance.
 const router = express.Router();
 
 // Route that returns a list of users.
+router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
 
+  res.json({
+    name: user.name,
+    username: user.username
+  })
+}))
 
 // Route that creates a new user.
 router.post('/users', asyncHandler(async (req, res) => {
@@ -18,7 +26,7 @@ router.post('/users', asyncHandler(async (req, res) => {
   } catch (error) {
     if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
       const errors = error.errors.map(err => err.message);
-      res.status(400).json({ errors });   
+      res.status(400).json({ errors });
     } else {
       throw error;
     }
